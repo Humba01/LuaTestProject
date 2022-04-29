f7928049ce9dfe4e28abc68d8273209d9e4ef8be
Humberto Bravo <humbandroid@gmail.com>
2022-04-28 22:23:53 -0300

    LuaTesterProject {HN Labs} - Testando a linguagem Lua.

---
+++ public/test/tester.lua
@@ -0,0 +1,51 @@
+--& Variáveis Globais
+K = 9;
+
+--& Variáveis locais
+local t = 4;
+local tester = "r";
+
+--s Função: teste
+--s Descrição: teste
+--s Parâmetros: teste
+function testador()
+  print("teste");
+end
+
+function testador_parametros(a, b, c)
+  print(a, b, c);
+end
+
+--< Biblioteca Math
+local x, y;
+math.abs( x );
+math.acos( x );
+math.asin( x );
+math.atan( x );
+math.atan2( x, y );
+math.ceil( x );
+math.cos( x );
+math.cosh( x );
+math.deg( x );
+math.exp( x );
+math.floor( x );
+math.fmod( x, y );
+math.frexp( x );
+math.huge;
+math.ldexp( x, y );
+math.log( x );
+math.log10( x );
+math.max( x, y );
+math.min( x, y );
+math.modf( x );
+math.pi;
+math.pow( x, y );
+math.rad( x );
+math.random( x );
+math.randomseed( x );
+math.sin( x );
+math.sinh( x );
+math.sqrt( x );
+math.tan( x );
+math.tanh( x );
+-->
